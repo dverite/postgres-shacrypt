62e05cb01a2788bf6ab671228ad370710604b5ce
Rafael Cossovan <navossoc@gmail.com>
2018-03-13 07:40:58 -0300

    Fix build on Alpine Linux.

--- sha512-crypt.c
+++ sha512-crypt.c
@@ -513,7 +513,7 @@ sha512_crypt_r (const char *key, const char *salt, char *buffer, int buflen)
 
   /* Now we can construct the result string.  It consists of three
      parts.  */
-  cp = __stpncpy (buffer, sha512_salt_prefix, MAX (0, buflen));
+  cp = stpncpy (buffer, sha512_salt_prefix, MAX (0, buflen));
   buflen -= sizeof (sha512_salt_prefix) - 1;
 
   if (rounds_custom)
@@ -524,7 +524,7 @@ sha512_crypt_r (const char *key, const char *salt, char *buffer, int buflen)
       buflen -= n;
     }
 
-  cp = __stpncpy (cp, salt, MIN ((size_t) MAX (0, buflen), salt_len));
+  cp = stpncpy (cp, salt, MIN ((size_t) MAX (0, buflen), salt_len));
   buflen -= MIN ((size_t) MAX (0, buflen), salt_len);
 
   if (buflen > 0)
