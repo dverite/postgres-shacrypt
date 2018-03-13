c469d224908165dd502b4cb925dc7a3014161881
Rafael Cossovan <navossoc@gmail.com>
2018-03-13 07:53:22 -0300

    Make the header files more similar.

--- sha256.h
+++ sha256.h
@@ -1,3 +1,6 @@
+#define SHA256_SALT_PREFIX "$5$"
+#define SHA256_ROUNDS_PREFIX "rounds="
+
 /* Structure to save state of computation between the single steps.  */
 struct sha256_ctx
 {
@@ -7,18 +10,6 @@ struct sha256_ctx
   uint32_t buflen;
   char buffer[128];	/* NB: always correctly aligned for uint32_t.  */
 };
-
-/* Maximum salt string length.  */
-#define SALT_LEN_MAX 16
-/* Default number of rounds if not explicitly specified.  */
-#define ROUNDS_DEFAULT 5000
-/* Minimum number of rounds.  */
-#define ROUNDS_MIN 1000
-/* Maximum number of rounds.  */
-#define ROUNDS_MAX 999999999
-
-#define SHA256_SALT_PREFIX "$5$"
-#define SHA256_ROUNDS_PREFIX "rounds="
 
 char *sha256_crypt_r (const char *key, const char *salt, char *buffer, int buflen);
 
